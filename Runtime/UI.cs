8ba2e6027618bc9c11e6a100c7884996958a24c2
gentlemanSoup <anders.horne@gmail.com>
2022-10-20 19:36:36 +0200

    Added some more UI-functions

--- Runtime/UI.cs
+++ Runtime/UI.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
 namespace Pixygon.Micro {
@@ -6,9 +7,19 @@ namespace Pixygon.Micro {
         [SerializeField] private GameObject _gameOverScreen;
         [SerializeField] private GameObject _menuScreen;
         [SerializeField] private Camera _uiCamera;
+        [SerializeField] private GameObject[] _lifeIcons;
+        [SerializeField] private TextMeshPro _coinText;
 
         public void Initialize() {
             MicroController._instance.Display._camera.GetUniversalAdditionalCameraData().cameraStack.Add(_uiCamera);
+        }
+        public void SetLife(int i) {
+            _lifeIcons[0].SetActive(i >= 0);
+            _lifeIcons[1].SetActive(i >= 1);
+            _lifeIcons[2].SetActive(i >= 2);
+        }
+        public void SetCoins(int coins) {
+            _coinText.text = $"Coins: {coins}";
         }
         public void TriggerMenuScreen(bool activate) {
             gameObject.SetActive(activate);
