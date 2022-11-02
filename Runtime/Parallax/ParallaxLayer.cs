a194a2056dcb2585878a294423d160b88e49ea13
gentlemanSoup <anders.horne@gmail.com>
2022-11-02 14:20:08 +0100

    asd

--- Runtime/Parallax/ParallaxLayer.cs
+++ Runtime/Parallax/ParallaxLayer.cs
@@ -46,8 +46,8 @@ namespace Pixygon.Micro.Parallax {
             
             transform.localPosition = new Vector3(newPos.x, newPos.y, _startZ);
 
-            if (temp > _startPos.x + (length / 32f)) _startPos = new Vector2(_startPos.x + (length/32f), _startPos.y);
-            else if (temp < _startPos.x - (length / 32f)) _startPos = new Vector2(_startPos.x - (length/32f), _startPos.y);
+            if (temp > _startPos.x + (length / 32f)) _startPos = new Vector2(_startPos.x + (length/16f), _startPos.y);
+            else if (temp < _startPos.x - (length / 32f)) _startPos = new Vector2(_startPos.x - (length/16f), _startPos.y);
         }
     }
 }
