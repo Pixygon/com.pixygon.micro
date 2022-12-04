a1b9a5b603b0929138f99571495363b9cb6bd8ee
gentlemanSoup <anders.horne@gmail.com>
2022-12-04 17:52:12 +0100

    Update ScoreManager.cs

--- Runtime/Levels/ScoreManager.cs
+++ Runtime/Levels/ScoreManager.cs
@@ -5,6 +5,7 @@ namespace Pixygon.Micro {
     public class ScoreManager : MonoBehaviour {
         private LevelLoader _levelLoader;
         private int _points;
+        private int _savedPoints;
 
         public void Initialize(LevelLoader loader) {
             _levelLoader = loader;
@@ -15,6 +16,15 @@ namespace Pixygon.Micro {
             _points += points;
             _levelLoader.Ui.SetPoints(_points);
             EffectsManager.SpawnScoreEffect(points, transform.position);
+        }
+
+        public void SetSavedPoints() {
+            _savedPoints = _points;
+        }
+
+        public void ResetPointsToSaved() {
+            _points = _savedPoints;
+            _levelLoader.Ui.SetPoints(_points);
         }
 
         private void ResetPoints() {
