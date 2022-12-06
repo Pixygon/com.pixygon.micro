51136e3c8f8ecef6b5fe773dd56ae0c8c999dfd9
gentlemanSoup <anders.horne@gmail.com>
2022-12-06 21:57:25 +0100

    Update Pickup.cs

--- Runtime/Pickup.cs
+++ Runtime/Pickup.cs
@@ -9,9 +9,12 @@ namespace Pixygon.Micro {
         [SerializeField] private UnityEvent _onRespawn;
         [SerializeField] private bool _destroyOnPickup;
         [SerializeField] private float _destroyTime;
+        [SerializeField] private bool _timeout;
+        [SerializeField] private float _timeoutTime = 8f;
         
         [field: SerializeField] public int PickupId { get; private set; }
-        
+
+        private float _timer;
         public bool Taken { get; private set; }
         public void TakePickup() {
             Taken = true;
@@ -24,6 +27,17 @@ namespace Pixygon.Micro {
         public void Respawn() {
             Taken = false;
             _onRespawn.Invoke();
+        }
+
+        private void Awake() {
+            if (_timeout)
+                _timer = _timeoutTime;
+        }
+
+        private void Update() {
+            if (!_timeout) return;
+            _timer -= Time.deltaTime;
+            if (_timer < 0f) Destroy(gameObject);
         }
     }
 }
