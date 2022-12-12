2b285581e901c26860089e34dcc61b0283e71309
gentlemanSoup <anders.horne@gmail.com>
2022-12-12 15:00:35 +0100

    AIReactions

--- Runtime/MicroActor/AIListenerData.cs
+++ Runtime/MicroActor/AIListenerData.cs
@@ -1,10 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Pixygon.Micro
 {
     public class AIListenerData : ScriptableObject
     {
+        public AIReaction _reaction;
+        public Color _color;
+        public bool _debugMode;
     }
 }
---
+++ Runtime/MicroActor/AIReaction.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pixygon.Micro
+{
+    public enum AIReaction
+    {
+        Turn,
+        Teleport,
+        Rush,
+        None
+    }
+}
