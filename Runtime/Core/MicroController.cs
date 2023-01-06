3dcd9a8b6081d3e21abea8a80e17eaf2d9e32c93
gentlemanSoup <anders.horne@gmail.com>
2023-01-06 01:14:19 +0100

    Update MicroController.cs

--- Runtime/Core/MicroController.cs
+++ Runtime/Core/MicroController.cs
@@ -90,7 +90,7 @@ namespace Pixygon.Micro
         public void TriggerHomeMenu(bool started) {
             if(!started) return;
             if (HomeMenuOpen) {
-                if (_cartridges.Length != 0 || CurrentlyLoadedCartridge != null || Wallet == string.Empty) {
+                if (_cartridges.Length != 0 && CurrentlyLoadedCartridge != null && Wallet != string.Empty) {
                     Debug.Log("Home menu should close...");
                     HomeMenuOpen = false;
                 }
