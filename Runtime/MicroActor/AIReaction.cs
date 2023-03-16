75b38a4f2f7d4c11cd7cd20c45687e5171e678cf
gentlemanSoup <anders.horne@gmail.com>
2023-03-16 15:32:35 +0100

    Fixed up fonts and did all the other stuff, cool!

--- Runtime/MicroActor/AIReaction.cs
+++ Runtime/MicroActor/AIReaction.cs
@@ -1,14 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace Pixygon.Micro
-{
-    public enum AIReaction
-    {
+namespace Pixygon.Micro {
+    public enum AIReaction {
         Turn,
         Teleport,
         Rush,
+        Attack,
         None
     }
 }
