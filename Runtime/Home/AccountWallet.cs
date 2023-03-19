3456348c3b165ddac4fe527e9a0a0606120d2b93
gentlemanSoup <anders.horne@gmail.com>
2023-03-19 15:19:41 +0100

    Small fixes to WalletScreen

--- Runtime/Home/AccountWallet.cs
+++ Runtime/Home/AccountWallet.cs
@@ -13,22 +13,32 @@ namespace Pixygon.Micro {
         [SerializeField] private GameObject _ethCheck;
         [SerializeField] private GameObject _tezCheck;
         [SerializeField] private GameObject _walletLoadingScreen;
+        [SerializeField] private GameObject _eventFetching;
 
-        /*
         public void GetWaxWallet() {
             MicroController._instance.GetWaxWallet();
             _walletLoadingScreen.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(_eventFetching);
         }
         public void GetEthWallet() {
             MicroController._instance.GetEthWallet();
             _walletLoadingScreen.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(_eventFetching);
         }
         public void GetTezWallet() {
             MicroController._instance.GetTezWallet();
             _walletLoadingScreen.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(_eventFetching);
         }
+        /*
         public void SetWaxWallet(string s) {
             _walletLoadingScreen.SetActive(false);
+            RefreshWallets();
+        }
+        */
+        public void WalletReceived() {
+            _walletLoadingScreen.SetActive(false);
+            EventSystem.current.SetSelectedGameObject(_eventWallet);
             RefreshWallets();
         }
         public void OpenWalletScreen() {
@@ -36,7 +46,6 @@ namespace Pixygon.Micro {
             EventSystem.current.SetSelectedGameObject(_eventWallet);
             RefreshWallets();
         }
-        */
 
         public void RefreshWallets() {
             _waxCheck.SetActive(!string.IsNullOrWhiteSpace(Saving.SaveManager.SettingsSave._user.waxWallet));
@@ -45,11 +54,6 @@ namespace Pixygon.Micro {
             _waxWalletText.text = Saving.SaveManager.SettingsSave._user.waxWallet;
             _ethWalletText.text = Saving.SaveManager.SettingsSave._user.ethWallet;
             _tezWalletText.text = Saving.SaveManager.SettingsSave._user.tezWallet;
-        }
-
-        public void WalletReceived() {
-            _walletLoadingScreen.SetActive(false);
-            RefreshWallets();
         }
     }
 }
