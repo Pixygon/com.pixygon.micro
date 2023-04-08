6dfd1dd7d52d429c93c627d0312e1495185ad775
gentlemanSoup <anders.horne@gmail.com>
2023-04-09 01:25:38 +0200

    Adding Difficulty!

--- Runtime/Levels/LevelLoader.cs
+++ Runtime/Levels/LevelLoader.cs
@@ -27,7 +27,10 @@ namespace Pixygon.Micro {
         public Level CurrentLevel { get; private set; }
         public LevelData CurrentLevelData { get; private set; }
         public Parallax.Parallax Parallax { get; private set; }
-        
+        public int Difficulty { get; private set; }
+        public void SetDifficulty(int difficulty) {
+            Difficulty = difficulty;
+        }
         public void OpenLoadingScreen() {
             if (_isLoading) return;
             _isLoading = true;
