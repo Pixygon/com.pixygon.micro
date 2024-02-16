557a58517982260468ec146689b3c8cb2c246ce0
gentlemanSoup <anders.horne@gmail.com>
2024-02-16 02:12:36 +0100

    Update ItemSpawner.cs

--- Runtime/ItemSpawners/ItemSpawner.cs
+++ Runtime/ItemSpawners/ItemSpawner.cs
@@ -6,13 +6,16 @@ namespace Pixygon.Micro {
         [SerializeField] private Vector2 _direction = Vector2.up;
         [SerializeField] private Vector2 _offset = Vector2.up;
         [SerializeField] private float _force = 10f;
+        [SerializeField] private bool _followParentRotation;
 
         public virtual void SpawnItem() {
-            var g = Instantiate(_itemPrefab, transform.position + (Vector3)_offset, Quaternion.identity);
+            var rotation = _followParentRotation ? transform.rotation : Quaternion.identity;
+            var g = Instantiate(_itemPrefab, transform.position + (Vector3)_offset, rotation);
             g.GetComponent<Rigidbody2D>().AddForce(_direction * _force, ForceMode2D.Impulse);
         }
         public virtual void SpawnItem(GameObject item) {
-            var g = Instantiate(item, transform.position + (Vector3)_offset, Quaternion.identity);
+            var rotation = _followParentRotation ? transform.rotation : Quaternion.identity;
+            var g = Instantiate(item, transform.position + (Vector3)_offset, rotation);
             g.GetComponent<Rigidbody2D>().AddForce(_direction * _force, ForceMode2D.Impulse);
         }
     }
