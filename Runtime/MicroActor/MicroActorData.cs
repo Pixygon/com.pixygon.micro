073901b00ec144d240b04ce43378fc602fb65599
gentlemanSoup <anders.horne@gmail.com>
2024-03-20 03:32:49 +0100

    Moved out Actor-stuff

--- Runtime/MicroActor/MicroActorData.cs
+++ Runtime/MicroActor/MicroActorData.cs
@@ -1,21 +1,9 @@
-﻿using Pixygon.Effects;
-using UnityEngine.AddressableAssets;
-using Pixygon.ID;
+﻿using Pixygon.Actors;
 using UnityEngine;
 
 namespace Pixygon.Micro {
     [CreateAssetMenu(menuName = "Pixygon/MicroActor", fileName = "New MicroActorData")]
-    public class MicroActorData : IdObject {
-        public AssetReference _actorRef;
-        public float _speed;
-        public int _hp;
+    public class MicroActorData : ActorData {
         public int _pointsValue;
-        public bool _isHostile;
-        public bool _isKillable;
-        public AIListenerData[] _listeners;
-        public bool _useIframes;
-        public float _iFrameLength = .6f;
-        public EffectData _damageFx;
-        public EffectData _deathFx;
     }
 }
