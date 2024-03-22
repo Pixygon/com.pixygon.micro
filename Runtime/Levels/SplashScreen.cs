37ceeeb790ce245a5c65ac18a2c51968a11003ed
gentlemanSoup <anders.horne@gmail.com>
2024-03-22 19:13:48 +0100

    Update SplashScreen.cs

--- Runtime/Levels/SplashScreen.cs
+++ Runtime/Levels/SplashScreen.cs
@@ -9,10 +9,12 @@ namespace Pixygon.Micro {
         [SerializeField] private int _splashDelay = 100;
 
         public async Task ShowSplashScreens(Action onFinish = null) {
+            _splashScreen.gameObject.SetActive(true);
             foreach (var s in _splashScreenSprites) {
                 _splashScreen.sprite = s;
                 await Task.Delay(_splashDelay);
             }
+            _splashScreen.gameObject.SetActive(false);
             onFinish?.Invoke();
         }
     }
