4e5565540ce4ca0de55d700c3b8beaa0ca5da69b
gentlemanSoup <anders.horne@gmail.com>
2024-09-27 12:58:51 +0200

    Update UI.cs

--- Runtime/UI.cs
+++ Runtime/UI.cs
@@ -10,6 +10,7 @@ namespace Pixygon.Micro {
         [SerializeField] private GameObject _levelEndScreen;
         [SerializeField] private GameObject _pregameScreen;
         [SerializeField] private GameObject[] _lifeIcons;
+        [SerializeField] private LifeStyle _lifeStyle = LifeStyle.Hearts;
         [SerializeField] private TextMeshPro _coinText;
         [SerializeField] private TextMeshPro _pointsText;
         [SerializeField] private TextMeshPro _levelStartText;
@@ -56,11 +57,19 @@ namespace Pixygon.Micro {
             _loadScreen.Activate(show);
         }
         public void SetLife(int i) {
-            _lifeIcons[0].SetActive(i >= 0);
-            _lifeIcons[1].SetActive(i >= 1);
-            _lifeIcons[2].SetActive(i >= 2);
+            switch (_lifeStyle) {
+                case LifeStyle.Hearts:
+                    for (var x = 0; x < _lifeIcons.Length; x++) {
+                        _lifeIcons[x].SetActive(i >= x);
+                    }
+                    break;
+                case LifeStyle.SpriteSwitch:
+                    for (var x = 0; x < _lifeIcons.Length; x++) {
+                        _lifeIcons[x].SetActive(i == x);
+                    }
+                    break;
+            }
         }
-
         public void SetCoins(int coins) {
             if(_animateCoins) _coinsAnimator.SetTrigger("Tick");
             _coinText.text = coins.ToString();
@@ -74,12 +83,10 @@ namespace Pixygon.Micro {
                 _powerupRenderer.gameObject.SetActive(true);
             }
         }
-
         public void SetPoints(int points) {
             if(_animatePoints) _pointsAnimator.SetTrigger("Tick");
             _pointsText.text = points.ToString();
         }
-
         public void SetCollectable(bool[] collectable) {
             if(_animateCollectables) _collectablesAnimator.SetTrigger("Tick");
             for (var i = 0; i < collectable.Length; i++) {
@@ -91,14 +98,18 @@ namespace Pixygon.Micro {
             if (MicroController._instance.CurrentlyLoadedCartridge != null)
                 _versionText.text = MicroController._instance.CurrentlyLoadedCartridge._version;
         }
-
         public void TriggerGameOverScreen(bool activate) {
             _gameOverScreen.SetActive(activate);
         }
-
         public void TriggerStartScreen(bool activate, string text = "") {
             _levelStartText.text = text;
             _levelStartScreen.SetActive(activate);
         }
+    }
+    public enum LifeStyle {
+        Hearts,
+        Counter,
+        Bar,
+        SpriteSwitch
     }
 }
